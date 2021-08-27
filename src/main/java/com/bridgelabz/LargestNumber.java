cdbfc864f0b5e5b5cbfc91ec063538832b5e4c91
ToshitaC <toshitachaudhari17@gmail.com>
2021-08-27 09:02:46 +0530

    Added:Even Odd Program

---
+++ src/main/java/com/bridgelabz/LargestNumber.java
@@ -0,0 +1,17 @@
+package com.bridgelabz;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 79;
+        int c = 23;
+        if (a >= b && a >= c)
+            System.out.println("a is the the largest number");
+        else if (b >= a && b >= c)
+        System.out.println("b is the the largest number");
+        else
+        System.out.println("c is the the largest number");
+
+
+    }
+}
---
+++ src/main/java/com/bridgelabz/QuotientRemainder.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+public class QuotientRemainder {
+    public static void main(String [] args){
+        int a = 89;
+        int b = 2;
+        int quotient = a / b;
+        int remainder = a % b;
+        System.out.println("The Quotient is:" + quotient);
+        System.out.println("The Remainder is:" + remainder);
+    }
+}
